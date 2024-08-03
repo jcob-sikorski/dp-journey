96847e6b45f215cee34b79e82f4d3434b3a27760
jcob-sikorski <jm.siekiera@gmail.com>
2024-08-03 17:15:05 +0200

    added gridTravelerIter

---
+++ gridTravelerIter.js
@@ -0,0 +1,20 @@
+const gridTraveler = (m, n) => {
+  // construct (m+1)*(n+1) size matrix
+  const table = Array(m + 1)
+    .fill( )
+    .map(() => Array(n + 1).fill(0));
+
+  table[1][1] = 1;
+
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= m; j++) {
+      const current = table[i][j];
+
+      if (j + 1 <= n) table[i][j + 1] += current;
+      if (i + 1 <= m) table[i + 1][j] += current;
+    }
+  }
+  return table[m][n];
+};
+
+console.log(gridTraveler(3, 2));
